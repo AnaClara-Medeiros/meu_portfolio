cf582be2a9f0ee6722ed927678301596a78b3d96
ana_medeiros <anaclaramedeiros33@gmail.com>
2024-08-29 20:50:45 -0300

    Corrigindo o arqv style_config para ver se funciona no online

--- app/modulos/style_config.py
+++ app/modulos/style_config.py
@@ -1,48 +1,24 @@
-#cores
-branco = "#ffffff"
-roxo_escuro = "4F124D"
-roxo_claro = "C4459F"
-
-# Estilo customizado para botões
-estilo = f'''
+estilo = '''
     <style>
-    
-    .title-text {{
-        color: {roxo_claro}
-    }}
-
-    #root > div:nth-child(1) > div.withScreencast > div > div > div > section > div.block-container.st-emotion-cache-1jicfl2.ea3mdgi5 {{
-        padding : 0.5rem 5rem;
-    }}
-
-    #root > div:nth-child(1) > div.withScreencast > div > div > header{{
-        background-color: transparent ;
-        position: relative; /* Necessário para o z-index funcionar */
-        z-index: 0;}}
-
-    .menu {{
-        background-color: transparent ;
-    }}
-
-    .st-emotion-cache-6qob1r {{
-        background-color: {roxo_escuro} !important;
-    }}
-    .stButton button {{
-        background-color: {roxo_escuro};
-        color: {branco};
-        padding: 10px;
-        margin: -5px 0;
-        border: 1px solid {branco};
-        border-radius: 20px;
-        cursor: pointer;
-        width: 100%;
-        display: flex;
-        justify-content: flex-start;
-    }}
-    .stButton button:hover {{
-        background-color: {roxo_claro};
-        border: 1px solid {roxo_claro};
-        color: {branco};
-    }}
+    .title-text {
+        color: #C4459F;
+    }
+    .stButton button {
+        background-color: #4F124D !important;
+        color: #ffffff !important;
+        padding: 10px !important;
+        margin: -5px 0 !important;
+        border: 1px solid #ffffff !important;
+        border-radius: 20px !important;
+        cursor: pointer !important;
+        width: 100% !important;
+        display: flex !important;
+        justify-content: flex-start !important;
+    }
+    .stButton button:hover {
+        background-color: #C4459F !important;
+        border: 1px solid #C4459F !important;
+        color: #ffffff !important;
+    }
     </style>
-    '''
+'''
