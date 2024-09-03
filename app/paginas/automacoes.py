437d2a4698b50568000c70f9590a315bd87bf464
ana_medeiros <anaclaramedeiros33@gmail.com>
2024-09-03 12:43:19 -0300

    Colocando link para o github

--- app/paginas/automacoes.py
+++ app/paginas/automacoes.py
@@ -1,7 +1,8 @@
 import streamlit as st
+from modulos.open_page_url import open_page
 
 def pag_automacoes():
-    st.write('automacoes AQUI')
+    open_page('https://github.com/AnaClara-Medeiros')
 
 if __name__ == '__main__':
     pag_automacoes()
