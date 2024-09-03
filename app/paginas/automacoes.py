8ccff8e66178b73b21f86f0e3aee5a7d388a3ea3
ana_medeiros <anaclaramedeiros33@gmail.com>
2024-09-03 13:13:04 -0300

    Colcoando st.session_satge para retorna a pagina produto

--- app/paginas/automacoes.py
+++ app/paginas/automacoes.py
@@ -1,8 +1,11 @@
 import streamlit as st
 from modulos.open_page_url import open_page
 
+
+
 def pag_automacoes():
     open_page('https://github.com/AnaClara-Medeiros')
 
-if __name__ == '__main__':
-    pag_automacoes()
+    if st.button("Voltar para Produtos"):
+        st.session_state.pag = 'produtos'
+
--- app/paginas/github.py
+++ app/paginas/github.py
@@ -4,5 +4,5 @@ from modulos.open_page_url import open_page
 def pag_github():
     open_page('https://github.com/AnaClara-Medeiros')
 
-if __name__ == '__main__':
-    pag_github()
+    if st.button("Voltar para Produtos"):
+        st.session_state.pag = 'produtos'
