3120545678475370b79aa96102ea75faf578643d
ana_medeiros <anaclaramedeiros33@gmail.com>
2024-10-25 15:25:13 -0300

    add tela de fundo na pagina de github

--- app/paginas/github.py
+++ app/paginas/github.py
@@ -1,7 +1,31 @@
 import streamlit as st
 from modulos.open_page_url import open_page
+from modulos.get_url_from_png import get_base64_of_png_file
+
 
 def pag_github():
+
+    bin_str = get_base64_of_png_file(r'app/outros/tela_fundo.png') #transformar um arqv local em html
+
+    estilo_pagina = """
+    <style>
+     [data-testid="stAppViewContainer"] {
+     background-image : url("data:image/png;base64,%s");
+     background-size : cover;
+     }
+ 
+     [data-testid="stHeader"] {
+     background-color : rgba(0,0,0,0);
+     }
+
+    #root > div:nth-child(1) > div.withScreencast > div > div > div > section.st-emotion-cache-vk3wp9.eczjsme11 > div.st-emotion-cache-6qob1r.eczjsme3 {
+      background-color: #000000;
+
+    </style>"""% bin_str
+
+
+    st.markdown(estilo_pagina, unsafe_allow_html=True)
+
     col1, col2, col3, col4 = st.columns(4, gap="medium")
     # Título da página
     col1.write(
