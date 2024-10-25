dac7050b9b21c7dd596d37aa62b3d0631dc12a10
ana_medeiros <anaclaramedeiros33@gmail.com>
2024-10-25 15:20:31 -0300

    Add página de apresentaçõpes

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ import style_config
 from streamlit_option_menu import option_menu
 from paginas.pagina_inicio import pag_inicio
 from paginas.dashboards import pag_dashboards
-from paginas.automacoes import pag_automacoes
+from paginas.apresentacoes import pag_apresentacoes
 from paginas.github import pag_github
 from paginas.pagina_produtos import main_pag_produtos
 
@@ -13,7 +13,7 @@ st.set_page_config(layout="wide", initial_sidebar_state = 'collapsed', page_titl
 estilo_html_css = style_config.estilo
 st.markdown(estilo_html_css, unsafe_allow_html=True) 
 
-selected = option_menu(None, ["Home", "DataViz/Dashboards","IA/Automações", "Participações","Certificados"], 
+selected = option_menu(None, ["Home", "DataViz/Dashboards","IA/Automações", "Apresentações","Certificados"], 
             icons=['house', 'file-bar-graph','robot', 'file-play','file-check'], menu_icon="cast", default_index=0, orientation='horizontal',
             styles={"container": {"padding": "0!important", "background-color": "transparent"}})
 
@@ -28,6 +28,8 @@ if selected == 'IA/Automações':
     st.session_state.pag = 'github'
     pag_github()
             
-
+if selected == 'Apresentações':
+    st.session_state.pag = 'apresentacao'
+    pag_apresentacoes()
 
  
---
+++ app/paginas/apresentacoes.py
@@ -0,0 +1,37 @@
+import streamlit as st
+from modulos.get_url_from_png import get_base64_of_png_file
+from modulos.open_page_url import open_page
+
+
+def pag_apresentacoes():
+    bin_str = get_base64_of_png_file(r'app/outros/pag_apresentacoes.png') #transformar um arqv local em html
+
+    estilo_pagina = """
+    <style>
+     [data-testid="stAppViewContainer"] {
+     background-image : url("data:image/png;base64,%s");
+     background-size : cover;
+     }
+ 
+     [data-testid="stHeader"] {
+     background-color : rgba(0,0,0,0);
+     }
+
+    #root > div:nth-child(1) > div.withScreencast > div > div > div > section.st-emotion-cache-vk3wp9.eczjsme11 > div.st-emotion-cache-6qob1r.eczjsme3 {
+      background-color: #000000;
+
+    </style>"""% bin_str
+
+    st.markdown(estilo_pagina, unsafe_allow_html=True)
+
+    col1, col2, col3 = st.columns(3, gap="large")
+
+    if col1.button('Palestra Business Inteligence - FATEC Assis'):
+        open_page('https://www.linkedin.com/feed/update/urn:li:activity:7253035103453364225/')
+
+
+    if col2.button('Minicurso - Introdução ao Python'):
+        open_page('https://www.youtube.com/playlist?list=PL2lhbF8mjkwioIrekH0JKyCMqDcsFE3if')
+
+    if col3.button('Congresso Ciência de Dados das Fatecs'):
+        open_page('https://www.linkedin.com/feed/update/urn:li:activity:6863625691813019648/')
